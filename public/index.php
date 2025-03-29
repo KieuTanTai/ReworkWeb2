b6f9b1350365c1421283a7726863658bf04da550
Thien_Lang <kieutantai2x@gmail.com>
2025-03-30 03:02:08 +0700

    init

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+     <meta name="description"
+          content="Shop Mobile " />
+     <link rel="shortcut icon" href="./assets/images/icons/web_logo/main-logo.ico" type="image/x-icon" />
+     <link rel="stylesheet" href="./assets/fonts/fontawesome-6.6.0/css/all.min.css" />
+     <link rel="stylesheet" href="./assets/css/index.css" />
+     <link rel="stylesheet" href="./assets/css/responsive.css" />
+</head>
+<body>
+     <div class="web-content">
+          <?php include "../app/views/header-footer.php"?>
+     </div>
+</body>
+</html>
