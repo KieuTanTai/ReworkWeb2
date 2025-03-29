5162fba075f0c55833cb7ecab3a0c3ab3e3c7dcc
Thien_Lang <kieutantai2x@gmail.com>
2025-03-30 03:33:19 +0700

    init

--- public/index.php
+++ public/index.php
@@ -6,13 +6,18 @@
      <meta name="description"
           content="Shop Mobile " />
      <link rel="shortcut icon" href="./assets/images/icons/web_logo/main-logo.ico" type="image/x-icon" />
-     <link rel="stylesheet" href="./assets/fonts/fontawesome-6.6.0/css/all.min.css" />
+     <link rel="stylesheet" href="./assets/fonts/fontawesome-6.6.0/css/all.min.css"/>
      <link rel="stylesheet" href="./assets/css/index.css" />
      <link rel="stylesheet" href="./assets/css/responsive.css" />
 </head>
 <body>
      <div class="web-content">
-          <?php include "../app/views/header-footer.php"?>
+          <?php 
+               include "../app/views/header-footer.php";
+               renderHeader();
+               renderSubHeader();
+               renderFooter();
+          ?>
      </div>
 </body>
 </html>
