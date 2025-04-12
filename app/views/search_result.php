e81b96009c4057353438a081868c37b82e3c3b9e
Thien_Lang <kieutantai2x@gmail.com>
2025-04-12 09:40:37 +0700

    add container for search result

--- app/views/search_result.php
+++ app/views/search_result.php
@@ -3,9 +3,22 @@ function renderSearch()
 {
      ?>
      <!-- search -->
-     <div id="search-content" class="grid-col col-l-12 col-m-12 col-s-12 disable">
+     <section id="search-content" class="container root-session-content grid-col col-l-12 col-m-12 col-s-12 disable">
+          <div class="category-tab">
+               <div class="heading">
+                    <div id="new-phone-label" class="heading-label"></div>
+                    <div class="uppercase font-bold font-size-20 padding-left-8">Mới ra mắt
+                    </div>
+               </div>
 
-     </div>
+               <!-- container for products -->
+               <div class="product-container"></div>
+
+               <div class="flex justify-center align-center font-bold capitalize margin-bottom-16">
+                    <a href="#" class="category-btn button">Xem thêm</a>
+               </div>
+          </div>
+     </section>
      <?php
 }
 ?>
