6b1a66042c5ec45bd95871ae5449b64b1f31e57e
Hào hàn <Haohan>
2025-05-04 23:49:27 +0700

    feat:them nut tat form

--- app/views/sua1.php
+++ app/views/sua1.php
@@ -73,18 +73,27 @@ if (!$product) {
                 <input type="text" class="form-control" aria-label="Username" aria-describedby="basic-addon1" name="cameratruoc" value="<?= $product->cameratruoc ?>">
             </div>
     
-            <div style="text-align: center; margin-top: 30px; margin-left: 20px; position:fixed; top:86%;left:40%;">
+            <div style="text-align: center; margin-top: 30px; margin-left: 20px; position:fixed; top:86%;left:30%;">
                 <input type="submit" class="btn btn-primary" value="CẬP NHẬT" style="width: 120px; height: 35px; font-size: 17px;font-weight: bold;">
             </div>
-        </form>
+       
+   
     </div>
-    
+    <div style="text-align: center; margin-top: 30px; margin-left: 100px; position:fixed; top:86%;left:40%;">
+        <button class="btn btn-danger" style="width: 70px; height: 35px; font-size: 17px; font-weight: bold;"
+    onclick="if(confirm('Bạn có chắc chắn muốn hủy không?')) window.location.href = 'product.php';">
+    HỦY
+</button>
+        </div>
+        
+        
     <div class="image-preview-container">
         <div id="imagePreview">
             <?php if (!empty($product->hinhanh)): ?>
                 <img src="<?= $product->hinhanh ?>" alt="Preview">
             <?php endif; ?>
         </div>
+        </form>
     </div>
 </div>
 
